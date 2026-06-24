cfba021e1d24b4615e4b9b4b72b2a5ec05ffc657
rasyidrizky <rasyid.rsn@gmail.com>
2026-06-24 15:59:01 +0700

    feat: create user model

--- index.ts
+++ index.ts
@@ -1,1 +1,24 @@
-console.log("Hello via Bun!");
+import express from 'express';
+
+const app = express();
+const port = 5000;
+
+import sequelize from './config/database';
+
+app.use(express.json());
+
+async function connect() {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully');
+
+        app.listen(port, () => {
+            console.log(`Server running on http://localhost:${port}`);
+            console.log(`API Documentation available at http://localhost:${port}/api-docs`);
+        });
+    } catch(error) {
+        console.error('Unable to run server:', error);
+    }
+}
+
+connect();
---
+++ models/User.ts
@@ -0,0 +1,31 @@
+import { DataType, Model } from "sequelize";
+import sequelize from "../config/database.ts";
+
+class User extends Model {}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        username: {
+            type: DataTypes.STRING, 
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING, 
+            allowNull: false,
+            unique: true,
+        }
+    },
+    {
+        sequelize,
+        modelName: 'User',
+        tableName: 'users',
+        freezeTableName: true
+    }
+);
+
+export default User;
