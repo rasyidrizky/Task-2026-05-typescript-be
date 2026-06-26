f47198f74676f492c0f656671896c3e8c18c2d38
rasyidrizky <rasyid.rsn@gmail.com>
2026-06-26 13:18:26 +0700

    fix: add express data type to error handler

--- middleware/errorHandler.ts
+++ middleware/errorHandler.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from "express";
+import { type Request, type Response, type NextFunction } from "express";
 
 function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
     console.error(err.stack);
